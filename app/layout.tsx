939e392d96459c654491ff4fe688a2f93226cf64
arr38126-cyber <arr38126@gmail.com>
2026-03-29 16:48:05 +0200

    Delete app directory

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import './globals.css'
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ar">
-      <body>{children}</body>
-    </html>
-  )
-}
