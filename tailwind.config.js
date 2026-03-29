9ff8771b4463f94cf572e5345ac228bcefa7fd58
arr38126-cyber <arr38126@gmail.com>
2026-03-29 16:00:27 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'luxury-brown': '#1a120b',
+        'gold-accent': '#d4af37'
+      }
+    }
+  }
+}
+
